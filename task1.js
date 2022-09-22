373290c450ed336e847024a8ac4d975c0259e864
Salz <74233105+salzteam@users.noreply.github.com>
2022-09-22 21:33:46 +0700

    Update task1.js

--- task1.js
+++ task1.js
@@ -8,19 +8,19 @@ const biodata = {
       schoolName: "TK PERTIWI",
       yearIn: 2009,
       yearOut: 2010,
-      major: "null",
+      major: null,
     },
     {
       schoolName: "SDN 3 GUMAWANG",
       yearIn: 2010,
       yearOut: 2016,
-      major: "null",
+      major: null,
     },
     {
       schoolName: "SMPN 1 BELITANG",
       yearIn: 2016,
       yearOut: 2019,
-      major: "null",
+      major: null,
     },
     {
       schoolName: "SMAN 1 BELITANG",
